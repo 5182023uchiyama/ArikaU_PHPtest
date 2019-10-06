ab5d179f059843c08c663e05c7936788a0df6039
5182023uchiyama <49850426+5182023uchiyama@users.noreply.github.com>
2019-10-06 21:28:13 +0900

    phptest登録

---
+++ study.localhost/index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 'hello php!!!';
+/*
+$randomNumber = rand();
+echo "乱数を表示", $randomNumber;
+$num1 = 123;
+$num2 = $num1;
+echo $num1,$num2;
+$num1 = 456;
+echo $num1,$num2;
+*/
+/*
+$numArray = array(1,2,3,4,5,6,7,8,9,10);
+foreach($numArray as $i ){
+    $num3 = $i * 10;
+    echo $i," ",$num3;
+}
+*/
+$hour = date('H');
+$hour2 = date('G');
+echo $hour, $hour2 ;
+var_dump($hour, $hour2);
